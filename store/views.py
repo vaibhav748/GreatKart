9c2b233853ebfe09cd089cf9d87e4f74b86b5eac
vaibhav881 <mvaibhav002@gmail.com>
2021-12-10 19:18:32 +0530

    Added review and rating functionality

--- store/views.py
+++ store/views.py
@@ -1,10 +1,13 @@
 from django.db.models import Q
-from django.shortcuts import render, get_object_or_404
-from .models import Product
+from django.shortcuts import render, get_object_or_404, redirect
+from .models import Product, ReviewRating
 from category.models import Category
 from carts.models import Cart, CartItem
 from carts.views import _cart_id
 from django.core.paginator import Paginator
+from .forms import ReviewRatingForm
+from django.contrib import messages
+
 # Create your views here.
 
 def store(request, category_slug=None):
@@ -62,3 +65,27 @@ def search(request):
                 'product_count': product_count
             }
     return render(request, 'store/store.html', context=data)
+
+
+def submit_review(request, product_id):
+    url = request.META.get('HTTP_REFERER')
+    if request.method == "POST":
+        try:
+            reviews = ReviewRating.objects.get(user__id=request.user.id, product__id=product_id)
+            form = ReviewRatingForm(request.POST, instance=reviews)
+            form.save()
+            messages.success(request, 'Thank you! Your review has been updated.')
+            return redirect(url)
+        except ReviewRating.DoesNotExist:
+            form = ReviewRatingForm(request.POST)
+            if form.is_valid():
+                data = ReviewRating()
+                data.subject = form.cleaned_data['subject']
+                data.rating = form.cleaned_data['rating']
+                data.review = form.cleaned_data['review']
+                data.ip = request.META.get('REMOTE_ADDR')
+                data.product_id = product_id
+                data.user = request.user
+                data.save()
+                messages.success(request, 'Thank you! Your review has been submitted.')
+                return redirect(url)
